8973c50c400f87833fed11a4380b209fae3f1b8e
lucastamura <lucas10.tamura@gmail.com>
2024-08-05 17:48:01 +0000

    Aula 05/08/2024

---
+++ app/src/main/java/application/controller/MensagemContoller.java
@@ -0,0 +1,9 @@
+package application.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController //Interface spring que identifique que a classe abaixo será um controlador rest
+public class MensagemContoller {
+
+    
+}
